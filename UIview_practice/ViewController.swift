01bbb7f994f4f85fe8432e9a54c5018252deee8c
Kim - leo <ochell2@naver.com>
2021-10-30 03:51:16 +0900

    Initial Commit

---
+++ UIview_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIview_practice
+//
+//  Created by 김승현 on 2021/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
